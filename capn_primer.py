908e20c8137c5a100d4f7b74cee769278328b8d7
Brian Hall <bhall7@hawaii.edu>
2014-05-09 12:41:34 -1000

    check dependencies and inputs

--- capn_primer.py
+++ capn_primer.py
@@ -2,7 +2,9 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import sys
+import os
 from src.dependency_checker import primer3_core_is_installed, blastall_is_installed
+from src.fasta_reader import FastaReader
 
 ## DEPENDENCIES: primer3_core, blast (?)
 ## data: fasta containing gene_ids and CDS 
@@ -13,11 +15,28 @@ from src.dependency_checker import primer3_core_is_installed, blastall_is_instal
 ## then can add in previous exploration steps.
 
 def main():
-    print("Yarr! I be Cap'n Primer.")
+    print("Yarr! I be Cap'n Primer.\n")
     print("Ahoy, let's check these scurvy dependencies...")
-
     check_dependencies()
 
+    # Verify files
+    print("Yarr! Now to verify the input files...")
+    target_fasta_path = 'targets.fasta'
+    genome_fasta_path = 'genome.fasta'
+    gff_path = 'genome.gff'
+
+    for path in [target_fasta_path, genome_fasta_path, gff_path]:
+        verify_path(path)
+
+    print("Shiver me timbers, the input files be present. Now I'll be reading them...")
+
+    # Read the fastas
+    fasta_reader = FastaReader()
+    target_file = open(target_fasta_path, 'rb')
+    target_seqs = fasta_reader.read(target_file)
+    #sys.stderr.write("Reading fasta...\n")
+    #self.read_fasta(fastapath)
+    #sys.stderr.write("Done.\n")
     # GIVEN: LIST OF GENE_IDS TO TARGET
     # GIVEN: FASTA WITH CDS FROM GENES OF INTEREST
     # GIVEN: GFF FOR GENOME IN QUESTION
@@ -43,18 +62,22 @@ def main():
 def check_dependencies():
     # check primer3_core
     if primer3_core_is_installed():
-        print("Yarr! primer3_core be installed.")
+        print("...Yarr! primer3_core be installed.")
     else:
         print("Yarr! Why ye not install primer3_core, scurvy dog.")
         sys.exit()
 
     # check blastall
     if blastall_is_installed():
-        print("Yarr! blastall be installed.")
+        print("...Yarr! blastall be installed.\n")
     else:
         print("Yarr! Why ye not install blastall, scurvy dog.")
         sys.exit()
 
+def verify_path(path):
+    if not os.path.isfile(path):
+        sys.stderr.write("Failed to find " + path + ". Walk the plank.\n")
+        sys.exit()
 
 if __name__ == '__main__':
     main()
