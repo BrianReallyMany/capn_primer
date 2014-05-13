d45a6d34aab2dba6533dedf940b9497f5ca51eb1
Brian Hall <bhall7@hawaii.edu>
2014-05-12 15:39:01 -1000

    boulder io reader now returning list of Primers

--- src/boulder_io_reader.py
+++ src/boulder_io_reader.py
@@ -1,59 +1,69 @@
 #!/usr/bin/env python
 
 import re
-from src.translator import reverse_complement
 from src.sequence import Sequence
+from src.primer import Primer
 
-class BoulderIOReader:
-
+class BoulderIOReader: 
     def __init__(self, primer_options=None):
         self.current_seq = None
 
-    def entry_to_primer_seqs(self, entry):
-        seqs = []
-        current_header = ""
-        current_primer = ""
+    def entry_to_primers(self, entry):
+        target_sequence = Sequence()
+        current_primer = Primer()
+        primers = []
         for line in entry.split('\n'):
             splitline = line.strip().split('=')
-            if not splitline[0]:
-                # end of entry
-                return seqs
+            if len(splitline) != 2:
+                # end of entry or something wrong...
+                # try to fail gracefully
+                return primers
             if splitline[0] == "SEQUENCE_ID":
-                current_header = splitline[1]
-                continue
-            elif "PRIMER_LEFT" in splitline[0] and "SEQUENCE" in splitline[0]:
-                # left primer sequence
-                # get sequence number:
-                m = re.search('[0-9]+', splitline[0])
-                if not m.group():
-                    continue
-                number = m.group(0)
-                # add primer sequence to current_primer
-                current_primer += splitline[1]
-                continue
-            elif "PRIMER_RIGHT" in splitline[0] and "SEQUENCE" in splitline[0]:
-                # right primer sequence
-                # get sequence number
-                m = re.search('[0-9]+', splitline[0])
-                if not m.group():
-                    continue
-                number = m.group(0)
-                current_primer += reverse_complement(splitline[1])
-                primer_header = current_header + "_primer_" + number
-                seqs.append(Sequence(primer_header, current_primer))            
-                current_primer = ""
-        return seqs
+                target_sequence.header = splitline[1]
+            elif "SEQUENCE_TEMPLATE" in splitline[0]:
+                target_sequence.bases = splitline[1]
+            elif "PRIMER_LEFT" in splitline[0]:
+                # match "PRIMER_LEFT_0" but not e.g. "PRIMER_LEFT_0_SEQUENCE"
+                m = re.search('[0-9]+$', splitline[0])
+                if m:
+                    number = m.group(0)
+                    current_primer.primer_name = "primer_" + str(number)
+                    current_primer.target_sequence = target_sequence
+                    start_and_length = splitline[1].split(',')
+                    start = start_and_length[0]
+                    length = start_and_length[1]
+                    current_primer.left_start = start
+                    current_primer.left_length = length
+            elif "PRIMER_RIGHT" in splitline[0]:
+                # match "PRIMER_LEFT_0" but not e.g. "PRIMER_LEFT_0_SEQUENCE"
+                m = re.search('[0-9]+$', splitline[0])
+                if m:
+                    start_and_length = splitline[1].split(',')
+                    start = start_and_length[0]
+                    length = start_and_length[1]
+                    current_primer.right_start = start
+                    current_primer.right_length = length
+                    # At this point we should have a complete primer
+                    if current_primer.primer_name and current_primer.target_sequence.bases:
+                        primers.append(current_primer)
+                        current_primer = Primer()
+                    else:
+                        sys.stderr.write("BoulderIOReader ERROR: reached end of primer info ")
+                        sys.stderr.write("but primer is incomplete:\nat this line:\n" + line + "\n")
+                        sys.stderr.write("Will discard this primer and attempt to continue.\n")
+                        current_primer = Primer()
+        return primers
 
     def read_primer3_output(self, io_buffer):
-        seqs = []
+        primers = []
         current_entry = ""
         for line in io_buffer:
             if line.strip() == "=":
                 # end of an entry
-                seqs.extend(self.entry_to_primer_seqs(current_entry))
+                primers.extend(self.entry_to_primers(current_entry))
                 current_entry = ""
             else:
                 current_entry += line
-        return seqs
+        return primers
 
 
